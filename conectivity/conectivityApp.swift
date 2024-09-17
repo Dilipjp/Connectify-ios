d6f1b75b753c5f4d608ce6b63ccba043006274af
Dilip <dilip@Dilips-MacBook-Air.local>
2024-09-17 02:37:16 -0400

    Initial project uploaded

---
+++ conectivity/conectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  conectivityApp.swift
+//  conectivity
+//
+//  Created by Dilip on 2024-09-17.
+//
+
+import SwiftUI
+
+@main
+struct conectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
