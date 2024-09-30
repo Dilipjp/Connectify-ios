014417a135f1ccf136c28ef0638053b7fb166d7b
Dilip <dilip@Dilips-MacBook-Air.local>
2024-09-29 21:15:10 -0400

    Firebase services added

--- conectivity/Views/AuthView.swift
+++ conectivity/Views/AuthView.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct AuthView: View {
+    @State private var showSignUp = false
+    @EnvironmentObject var authViewModel: AuthViewModel
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                if showSignUp {
+                    SignUpView(showSignUp: $showSignUp)
+                        .environmentObject(authViewModel)
+                } else {
+                    SignInView(showSignUp: $showSignUp)
+                        .environmentObject(authViewModel)
+                }
+            }
+        }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+            .environmentObject(AuthViewModel())
+    }
+}
