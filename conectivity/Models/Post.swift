6cbae81779520b2dc215c3832fa691d5176f0ec6
Dilip <32507121+Dilipjp@users.noreply.github.com>
2024-10-15 23:17:29 -0400

    Comment function finished

--- conectivity/Models/Post.swift
+++ conectivity/Models/Post.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
+struct Post: Identifiable {
+    var postId: String
+    var userId: String
+    var postImageUrl: String
+    var caption: String
+    var likeCount: Int
+    var commentCount: Int
+    var timestamp: Double
+    var likedByCurrentUser: Bool
+    var userData: UserData? // Optional property to hold user data
+    
+    var id: String { postId }
+}
+
+struct UserData {
+    var userName: String
+    var profileImage: UIImage
+}
+
