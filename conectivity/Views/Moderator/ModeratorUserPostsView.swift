bcb5e5418a23a4196f963a91d1f7dfe36b92931f
subash <gaddamsubashreddy@gmail.com>
2024-10-31 01:12:55 -0400

    create moderatoruserpostview

--- conectivity/Views/Moderator/ModeratorUserPostsView.swift
+++ conectivity/Views/Moderator/ModeratorUserPostsView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
-struct ModeratorUserPostsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-#Preview {
-    ModeratorUserPostsView()
-}
+public struct ModeratorUserPostsView: View {
+    public let userId: String
+
+    @State private var userPosts: [Post] = []
+    @State private var isLoading = true
+    @State private var selectedPost: Post? = nil
+    @State private var showEditPostView = false
+    @State private var showWarningAlert = false
+    @State private var warningMessage = "Please review the content of this post."
+
+    private var dbRef = Database.database().reference()
+
+    public init(userId: String) {
+        self.userId = userId
+    }
+
+    public var body: some View {
+        VStack {
+            if isLoading {
+                ProgressView("Loading posts...")
+            } else if userPosts.isEmpty {
+                Text("No posts available")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            } else {
+                List {
+                    ForEach(userPosts, id: \.postId) { post in
+                        VStack(alignment: .leading, spacing: 15) {
+                            // Post Caption
+                            Text(post.caption)
+                                .font(.headline)
+                        
