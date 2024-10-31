c0641041dd6498fbd61cf21e032615a038c59c4e
Santhosh Nallapati <santhoshnallapati@Santhoshs-Laptop.local>
2024-10-31 00:32:05 -0400

    Moderator can see all users

--- conectivity/Views/Moderator/ModeratorUsersView.swift
+++ conectivity/Views/Moderator/ModeratorUsersView.swift
@@ -6,12 +6,49 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
+
+struct User1: Identifiable {
+    var id: String { userId }
+    let userId: String
+    var userName: String
+    var userProfileImage: String
+    var userStatus: String
+}
 
 struct ModeratorUsersView: View {
+    @State private var allUsers: [User1] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 15) {
+                    ForEach(allUsers) { user in
+                        VStack {
+                            HStack(spacing: 15) {
+                                // User Profile Image with Loading & Error Handling
+                                AsyncImage(url: URL(string: user.userProfileImage)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 60, height: 60)
+                                        .clipShape(Circle())
+                                } placeholder: {
+                                    ProgressView()
+                                        .frame(width: 60, height: 60)
+                                        .background(Color.gray.opacity(0.3))
+                                        .clipShape(Circle())
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
+    
 }
+                
 
 #Preview {
     ModeratorUsersView()
