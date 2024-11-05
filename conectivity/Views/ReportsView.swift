3358e5a19214389cba8939ae803871ecf5d1a8cf
subash <gaddamsubashreddy@gmail.com>
2024-11-05 16:31:15 -0500

    created Identifiabel class report

--- conectivity/Views/ReportsView.swift
+++ conectivity/Views/ReportsView.swift
@@ -5,3 +5,23 @@
 //  Created by Subash Gaddam on 2024-11-05.
 //
 
+struct Report: Identifiable {
+    var id: String
+    var postId: String
+    var reason: String
+    var timestamp: TimeInterval
+    var reporterId: String
+    var caption: String?
+    var uploaderName: String?
+    var reporterName: String?
+    var postImageUrl: String?
+}
+
+struct ReportsView: View {
+    var body: some View {
+        
+    }
+}
+#Preview {
+    ReportsView()
+}
